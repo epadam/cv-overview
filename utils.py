7e354d3a162c628ff9e07afe2297cf2b1dc0b9fd
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-11-15 10:01:26 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+class REGISTRY:
+    inited = False
+    @classmethod
+    def get_metrics(cls):
+        if cls.inited:
+            return cls
+        else:
+            # Register your metrics here
+            cls.REQUEST_TIME = Summary('some_summary', 'Time spent in processing request')
+            cls.inited = True
