ec38ea754c2c6707bbee8cf19cb303064f0752bc
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-11-15 11:23:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
-from utils import REGISTRY
 
-METRICS = REGISTRY.get_metrics()
-
-@METRICS.REQUEST_TIME.time()
-def i_sleep():
-    sleep(1.0)
 
 st.write('GO')
 
-i_sleep()
+
