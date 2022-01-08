e7d20d624b36f589d96f0073c9a2e32ef32b58ea
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2022-01-09 00:39:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ if image_file is not None:
     st.write(file_details)
     img = load_image(image_file)
     st.image(img,height=250,width=250)
-    file_name = image_file.name
-    with open(file_name ,"wb") as f: 
+    
+    with open(image_file.name,"wb") as f: 
       f.write(image_file.getbuffer())         
     st.success("Saved File")
     
     
 reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
 
-result = reader.readtext(file_name)
+result = reader.readtext(image_file.name)
 
 st.write(result)
